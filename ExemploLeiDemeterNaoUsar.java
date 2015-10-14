b4fc3049fe1e29f891571cec9a46344a7189cda0
Lizandra Ottmann <lizandraottmann@gmail.com>
2015-10-14 19:31:50 -0300

    Lei Demeter Não usar

---
+++ ExemploLeiDemeterNaoUsar.java
@@ -0,0 +1,22 @@
+package exemplo.com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoCompras{
+
+	private List<Itens> itens;
+	
+	public CarrinhoCompras(){
+		this.itens = new ArrayList<>(Itens);
+		}
+	
+	public Double valorTotalDosItens(){
+		Double valorTotal = 0.0;
+		for(Itens item : itens){
+			valorTotal += item.getProduto().getValor();			
+		}
+		return valorTotal;
+	}
+	
+}
